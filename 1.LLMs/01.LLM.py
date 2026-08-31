12d452461d4e48f390768586a83814a125e3ccd2
sounakpradhan1980-rgb <sounakpradhan1980@gmail.com>
2026-08-31 23:18:44 +0530

    Add: llm

---
+++ 1.LLMs/01.LLM.py
@@ -0,0 +1,8 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+llm=OpenAI(model="gpt-3.5-turbo")
+result=llm.invoke("What is the capital of India?")
+print(result)
