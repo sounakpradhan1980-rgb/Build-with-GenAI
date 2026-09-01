8af01bad545b0bd25d8fe5fb3f09c9713d81a2b5
sounakpradhan1980-rgb <sounakpradhan1980@gmail.com>
2026-09-01 20:55:22 +0530

    modify Add deepseek chatmodel

--- 2.ChatModels/04.Chatmodel_hf.py
+++ 2.ChatModels/04.Chatmodel_hf.py
@@ -1,7 +1,12 @@
-from langchain_huggingface import chatHuggingFace,HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 
 load_dotenv()
 llm=HuggingFaceEndpoint(
-    repi_id=
+    repo_id="deepseek-ai/DeepSeek-R1-0528",
+    task="text-generation"
 )
+
+model= ChatHuggingFace(llm=llm)
+result=model.invoke("Hello, how are you?")
+print(result.content)
